47ef146fc7e86e93c5a9217d1ef8f114b91e122e
Daniel Jankelow <danielj@onecart.co.za>
2023-12-18 17:23:07 +0200

    re order images on home screen

--- src/components/DualFeatureBox.tsx
+++ src/components/DualFeatureBox.tsx
@@ -7,10 +7,8 @@ const DualFeatureBox: React.FC = () => {
         <div className="container my-5">
             {/* Row with image left aligned and text right aligned */}
             <div className="row align-items-center feature-row">
-                <div className="col-md-6 col-sm-12">
-                    <img src={leftAlignedImage} alt="Left aligned" className="img-fluid"/>
-                </div>
-                <div className="col-md-6 col-sm-12">
+               
+                <div className="col-md-6 col-sm-12 order-md-1">
                     <h2>KYC – Know Your Client</h2>
                     <hr className="title-underline" />
                     <p>Identity Document Verification</p>
@@ -20,14 +18,15 @@ const DualFeatureBox: React.FC = () => {
                         <p>AML Screening: PEPs, sanctions, watchlists and adverse media</p>
                         <p>Email Verification</p>
                         <p>Phone Verification</p>                </div>
+                        <div className="col-md-6 col-sm-12">
+                    <img src={leftAlignedImage} alt="Left aligned" className="img-fluid"/>
+                </div>
             </div>
             
             {/* Row with text left aligned and image right aligned */}
             <div className="row align-items-center feature-row">
-                <div className="col-md-6 col-sm-12 order-md-2 order-sm-1">
-                    <img src={rightAlignedImage} alt="Right aligned" className="img-fluid"/>
-                </div>
-                <div className="col-md-6 col-sm-12 order-md-1 order-sm-2">
+
+                <div className="col-md-6 col-sm-12 order-md-1 order-sm-1">
                     <h2>KYB – Know Your Businesss</h2>
                     <hr className="title-underline" />
                     <p>Automated Corporate Registry Check</p>
@@ -35,6 +34,10 @@ const DualFeatureBox: React.FC = () => {
                         <p>Ownership and Management Structure</p>
                         <p>Intermediate Shareholder Check</p>
                         <p>Ongoing Corporate AML Monitoring</p>
+                </div>
+
+                <div className="col-md-6 col-sm-12 order-md-2 order-sm-2">
+                    <img src={rightAlignedImage} alt="Right aligned" className="img-fluid"/>
                 </div>
             </div>
         </div>

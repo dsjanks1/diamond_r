5f6cd3668b00ebc6178c2d05c4707c206f4db88b
Daniel Jankelow <danielj@onecart.co.za>
2023-12-25 15:20:03 +0200

    about page updates and images

--- src/components/FeatureBox.tsx
+++ src/components/FeatureBox.tsx
@@ -1,30 +1,27 @@
 import React from 'react';
 import '../styles/FeatureBox.css'; // Make sure to create and import a CSS file for custom styling
-import imageOne from '../assets/diamondTerq.jpg'; // Update with the path to your image
-import imageTwo from '../assets/diamondSandBack.jpg'; // Update with the path to your image
+import imageOne from '../assets/lifescycle.png'; // Update with the path to your image
+import imageTwo from '../assets/peeling-useless-parts-jewelry.jpg'; // Update with the path to your image
 
 const FeatureBox: React.FC = () => {
     return (
         <div className="container">
         <div className="row justify-content-center">
             {/* The use of offset-md here assumes Bootstrap 4+ */}
-            <div className="col-lg-5 col-md-5 feature-box">
-                <img src={imageOne} alt="Our Mission" className="img-responsive"/>
+            <div className="col-lg-5 col-md-5 feature-box order-sm-1">
+            <h3 className='text-dark'>Our Legacy</h3>
+                <p className='text-grey-light'>Founded on a legacy of experience spanning three decades, verified by VDV was built to be at the forefront of compliance for the diamond industry. Our journey is rooted in a deep understanding of the industry's intricacies, evolving alongside the dynamic landscape to become a trusted partner for diamond businesses seeking excellence in compliance.</p>
+            <div className='hr-line-container'>
+                <div className="hr-line-2"></div>
+            </div>
                 <h3 className='text-dark'>Our Mission</h3>
                 <p className='text-grey-light'>Making Compliance Easy, Ensuring Security
                     Our mission is clear: to make compliance easy for the diamond industry. We do this by combining our extensive knowledge of regulatory frameworks with cutting-edge technology. Our aim is to provide a secure foundation for trading, allowing our clients to operate confidently in a world that has growing demands for the utmost in accountability and transparency.</p>
-                {/* <a href="#" className="discover-more">Discover More</a> */}
 
-                {/* <h2>Our Mission</h2>
-                    <p>Making Compliance Easy, Ensuring Security
-                    Our mission is clear: to make compliance easy for the diamond industry. We do this by combining our extensive knowledge of regulatory frameworks with cutting-edge technology. Our aim is to provide a secure foundation for trading, allowing our clients to operate confidently in a world that has growing demands for the utmost in accountability and transparency.
-                    </p> */}
             </div>
             
-            <div className="col-lg-5 col-md-5 feature-box">
-                <img src={imageOne} alt="KYB – Know Your Business" className="img-responsive"/>
-                <h3 className='text-dark'>Our Legacy</h3>
-                <p className='text-grey-light'>Founded on a legacy of experience spanning three decades, verified by VDV was built to be at the forefront of compliance for the diamond industry. Our journey is rooted in a deep understanding of the industry's intricacies, evolving alongside the dynamic landscape to become a trusted partner for diamond businesses seeking excellence in compliance.</p>
+            <div className="col-lg-5 col-md-5 feature-box order-sm-2">
+            <img src={imageOne} alt="Our Mission" className="img-responsive"/>          
                 
                 </div>
         </div>
--- src/pages/AboutUs.tsx
+++ src/pages/AboutUs.tsx
@@ -7,7 +7,7 @@ const AboutUs: React.FC = () => {
             {/* Hero Section */}
             <section className="about-us-hero d-flex justify-content-center text-white">
             <div className="row align-items-center feature-row">
-                <div className="col-md-4 col-sm-12">
+                <div className="col-md-10 col-sm-12">
                     {/* <img src={leftAlignedImage} alt="Left aligned" className="img-fluid"/> */}
                     <div className='about-us-text-container'>
                     <h1>About Verified by VDV</h1>
@@ -23,28 +23,15 @@ const AboutUs: React.FC = () => {
                <div className='mission-container'>
                 <FeatureBox></FeatureBox>
                </div>
-                {/* <div className="container position-relative text-green">
-                    <h2>Our Mission</h2>
-                    <p>Making Compliance Easy, Ensuring Security
-                    Our mission is clear: to make compliance easy for the diamond industry. We do this by combining our extensive knowledge of regulatory frameworks with cutting-edge technology. Our aim is to provide a secure foundation for trading, allowing our clients to operate confidently in a world that has growing demands for the utmost in accountability and transparency.
-                    </p>
-                </div> */}
+
             </section>
 
-            {/* Legacy Section */}
-            {/* <section className="about-us-legacy text-center py-5">
-                <div className="container">
-                    <h2>Our Legacy</h2>
-                    <p>Founded on a legacy of experience spanning three decades, verified by VDV was built to be at the forefront of compliance for the diamond industry. Our journey is rooted in a deep understanding of the industry's intricacies, evolving alongside the dynamic landscape to become a trusted partner for diamond businesses seeking excellence in compliance.</p>
-                </div>
-            </section> */}
-
             {/* Expertise Section with Interactive Cards */}
-            <section className="about-us-expertise py-5">
-            <div className="text-white legacy-container bg-dark">
+            <section className="about-us-expertise">
+            <div className="text-white legacy-container">
                 <div className="home-content">
-                    <h3 className="home-light">Unparalleled Expertise</h3>
-                    <p className="home-description">
+                    <h3 className="unparalleled-heading">Unparalleled Expertise</h3>
+                    <p className="unparalleled-description">
                     At verified by VDV, our team brings together a wealth of experience in finance, accounting, legal and technology. Our professionals are not just experts in their respective fields but also possess a keen understanding of the nuances of the diamond industry. This unique blend of skills positions us as a versatile and reliable partner, capable of navigating the complex intersection of regulation and the diamond industry.
                     </p>
                 </div>
